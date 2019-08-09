1bf868c37aa092adf052ee569ae9b852c860929e
yamine122 <wjdtmd92@gmail.com>
2019-08-09 15:43:35 +0900

    190809 15:40

--- java-basic/Hello.java
+++ java-basic/Hello.java
@@ -1,5 +1,5 @@
 class Hello{
     public static void main(String[] args){
-        System.out.print("Hello �ڹ� !!");
+        System.out.print("Hello Java !!");
     }
 }
--- java-basic/WhatName.java
+++ java-basic/WhatName.java
@@ -1,9 +1,9 @@
 /**
- * °ªÀ» ÀÔ·Â¹Þ¾Æ¼­ Ãâ·ÂÇÏ´Â ¿¹Á¦
+ * ï¿½ï¿½ï¿½ï¿½ ï¿½Ô·Â¹Þ¾Æ¼ï¿½ ï¿½ï¿½ï¿½ï¿½Ï´ï¿? ï¿½ï¿½ï¿½ï¿½
  */
 
 class WhatName {
     public static void main(String[] args) {
-         System.out.print("ÀÚ¹Ù"); 
+         System.out.print("ÀÌ¸§ÀÌ ¹¹´Ï?"); 
     }
 }
